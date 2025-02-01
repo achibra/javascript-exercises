799d49719d4edfb52dd175f210d627a24c5f31e0
achibra <154708772+achibra@users.noreply.github.com>
2025-02-01 21:10:45 +0100

    Finished seventh JS practice project

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(FHValue) {
+  let C = (FHValue - 32) * (5 / 9);
+  if (C === 0) {
+    return C;
+  } else {
+    let result = C.toFixed(1);
+    return result;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(CValue) {
+  let F = (CValue * (9 / 5)) + 32;
+  if (F === 0) {
+    return F;
+  } else {
+    let result = F.toFixed(1);
+    return result;
+  }
 };
 
 // Do not edit below this line
