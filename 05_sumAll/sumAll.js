ae8f93b8f67d67fa94c62590c7fb1a3a797d1c8a
achibra <154708772+achibra@users.noreply.github.com>
2025-02-01 20:56:13 +0100

    Finished fifth JS practice project

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(firstnumber, secondnumber) {
+    let sum = 0;
+    for (let i = firstnumber; i <= secondnumber; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
